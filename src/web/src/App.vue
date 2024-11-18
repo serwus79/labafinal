e12e925651b77706d8d6a485a381d38ef029ba12
Krzysztof Chomicz <krzysiek@chomicze.com>
2024-11-18 22:12:47 +0100

    init

---
+++ src/web/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const message = ref('');
+
+onMounted(async () => {
+  const response = await fetch('http://localhost:5000/api/message');
+  const data = await response.json();
+  message.value = data.message;
+});
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>Minimal API Response</h1>
+      <p>{{ message }}</p>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
